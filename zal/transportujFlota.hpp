27a00b66584d9255a3c9e680f261e5b612f26742
dawidurbanski4 <urbdadylbod@gmail.com>
2020-11-21 17:48:26 +0000

    solved3

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -8,13 +8,15 @@ unsigned int transportujFlota(unsigned int towar)
 
     if(towar == 0){
       return 0;
+      
     }else{
+
       int ilosc_towaru =0;
 
       while (ilosc_towaru < towar){
         Stocznia stocznia{};
         Statek* s1 = stocznia();
-        ilosctowaru = ilosc_towaru + s1->transportuj();
+        ilosc_towaru = ilosc_towaru + s1->transportuj();
 
         if (s1 == dynamic_cast<Zaglowiec*>(s1)){
           liczba_zaglowcow++;
